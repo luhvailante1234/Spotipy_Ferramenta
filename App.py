e0a7c0144bf8e7b5fd1b9ae7021d65edd3683987
Vick1608 <vicvaldirene@gmail.com>
2023-11-15 17:36:16 -0300

    Arquivo excluido

--- App.py
+++ App.py
@@ -1,9 +1,4 @@
-from flask import Flask, render_template
-from Top50Graficos import gerar_graficoTop50
-from Top100Graficos import gerar_graficoTop100
-from TopBrasilGraficos import gerar_graficoTopBrasil
-
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from Top50Graficos import gerar_graficoTop50
 from Top100Graficos import gerar_graficoTop100
 from TopBrasilGraficos import gerar_graficoTopBrasil
@@ -14,20 +9,24 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', grafico1=False, grafico2=False, grafico3=False)
 
+@app.route('/grafico')
+def exibir_grafico():
+    return render_template('grafico.html')
+
 @app.route('/gerar_graficoTop50')
 def gerar_e_mostrar_grafico():
     gerar_graficoTop50()
-    return render_template('index.html', grafico1=True, grafico2=False, grafico3=False)
+    return redirect(url_for('exibir_grafico', filename='top50.html'))
 
 @app.route('/gerar_graficoTop100')
 def gerar_e_mostrar_grafico2():
     gerar_graficoTop100()
-    return render_template('index.html', grafico1=False, grafico2=True, grafico3=False)
+    return redirect(url_for('exibir_grafico'))
 
 @app.route('/gerar_graficoTopBrasil')
 def gerar_e_mostrar_grafico3():
     gerar_graficoTopBrasil()
-    return render_template('index.html', grafico1=False, grafico2=False, grafico3=True)
+    return redirect(url_for('exibir_grafico'))
 
 if __name__ == '__main__':
     app.run(debug=True)
