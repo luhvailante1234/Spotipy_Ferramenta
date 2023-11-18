9996eebc0f150c35f587e58cde2e0cb77ceca11d
Vick1608 <vicvaldirene@gmail.com>
2023-11-18 00:59:15 -0300

    Commit of2

--- VariacaoPopularidadeTop50.py
+++ VariacaoPopularidadeTop50.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from matplotlib.patches import Patch
 from flask import Flask, render_template, redirect, url_for
+from matplotlib.patches import Patch
 
 app = Flask(__name__)
 
@@ -27,15 +27,14 @@ def gerar_grafico_variacao_popularidade_Top50():
     # Criar uma paleta de cores
     colors = sns.color_palette('viridis', len(top_10))
 
-    # Criar o gráfico de dispersão com legenda de cores
+    # Criar o gráfico de linha
     plt.figure(figsize=(12, 6))
-    scatter = plt.scatter(top_10['name_track'], top_10['popularity'], c=colors, label='Popularidade')
-
-    # Adicionar quadrados coloridos à legenda
-    legend_handles = [Patch(color=color, label=name) for name, color in zip(top_10['name_track'], colors)]
+    
+    for i, (name, color) in enumerate(zip(top_10['name_track'], colors)):
+        plt.plot([name], [top_10.loc[top_10['name_track'] == name, 'popularity']], marker='o', color=color, label=name)
 
     # Adicionar legenda com as 10 músicas mais populares fora do gráfico
-    plt.legend(handles=legend_handles, title='Legenda', bbox_to_anchor=(1.05, 1), loc='upper left', fontsize=10, title_fontsize=12)
+    plt.legend(title='Legenda', bbox_to_anchor=(1.05, 1), loc='upper left', fontsize=10, title_fontsize=12)
 
     # Adicionar um título e labels aos eixos
     plt.title(f"Top 10 Músicas Mais Populares em {data_especifica}", fontsize=17)
@@ -55,3 +54,4 @@ def gerar_grafico_variacao_popularidade_Top50():
 
     # Exibir o gráfico
     plt.show()
+
