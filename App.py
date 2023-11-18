4a447a531ad7d3d1a2fd9953b8d763dd789d8c81
Vick1608 <vicvaldirene@gmail.com>
2023-11-17 23:07:18 -0300

    Commit grafico novo

--- App.py
+++ App.py
@@ -3,23 +3,25 @@ from GeneroXMediaTop50 import gerar_grafico_popularidade_media_por_genero_Top50
 from GeneroXPopularidadeTop50 import gerar_grafico_popularidade_genero_Top50
 from VariacaoPopularidadeTop50 import gerar_grafico_variacao_popularidade_Top50
 from PopularidadeXArtistaTop50 import gerar_grafico_popularidade_artista_Top50
-from DuracaoMusicasTop50 import  gerar_grafico_duracao_musicas_Top50
+from DuracaoMusicasTop50 import gerar_grafico_duracao_musicas_Top50
 
 from GeneroXMediaTop100 import gerar_grafico_popularidade_media_por_genero_Top100
 from GeneroXPopularidadeTop100 import gerar_grafico_popularidade_genero_Top100
 from VariacaoPopularidadeTop100 import gerar_grafico_variacao_popularidade_Top100
 from PopularidadeXArtistaTop100 import gerar_grafico_popularidade_artista_Top100
+from DuracaoMusicasTop100 import gerar_grafico_duracao_musicas_Top100
 
 from GeneroXMediaTopBrasil import gerar_grafico_popularidade_media_por_genero_TopBrasil
 from GeneroXPopularidadeTopBrasil import gerar_grafico_popularidade_genero_TopBrasil
 from VariacaoPopularidadeTopBrasil import gerar_grafico_variacao_popularidade_TopBrasil
 from PopularidadeXArtistaTopBrasil import gerar_grafico_popularidade_artista_TopBrasil
+from DuracaoMusicasTopBrasil import gerar_grafico_duracao_musicas_TopBrasil
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', grafico1=False, grafico2=False, grafico3=False)
+    return render_template('index.html', grafico1=False, grafico2=False, grafico3=False, grafico4=False, grafico5=False, grafico6=False, grafico7=False, grafico8=False, grafico9=False, grafico10=False, grafico11=False, grafico12=False, grafico13=False, grafico14=False, grafico15=False)
 
 @app.route('/grafico')
 def exibir_grafico():
@@ -40,7 +42,8 @@ def gerar_e_mostrar_grafico2():
     gerar_grafico_popularidade_genero_Top100()
     gerar_grafico_variacao_popularidade_Top100()
     gerar_grafico_popularidade_artista_Top100()
-    return render_template('grafico.html', grafico5=True, grafico6=True, grafico7=True, grafico8=True)
+    gerar_grafico_duracao_musicas_Top100()
+    return render_template('grafico.html', grafico5=True, grafico6=True, grafico7=True, grafico8=True, grafico14=True)
 
 @app.route('/gerar_graficoTopBrasil')
 def gerar_e_mostrar_grafico3():
@@ -48,7 +51,8 @@ def gerar_e_mostrar_grafico3():
     gerar_grafico_popularidade_genero_TopBrasil()
     gerar_grafico_variacao_popularidade_TopBrasil()
     gerar_grafico_popularidade_artista_TopBrasil()
-    return render_template('grafico.html', grafico9=True, grafico10=True, grafico11=True, grafico12=True)
+    gerar_grafico_duracao_musicas_TopBrasil()
+    return render_template('grafico.html', grafico9=True, grafico10=True, grafico11=True, grafico12=True, grafico15=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ DuracaoMusicasTop100.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from flask import Flask, render_template, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def gerar_grafico_duracao_musicas_Top100():
+    # Leitura do arquivo CSV
+    df = pd.read_csv('spotifycharts-5YRmjoCTiI6uPGJAevX87A.csv')
+
+    # Extraindo a coluna de duração das músicas e convertendo para segundos
+    duracao_musica_segundos = df['duration_ms'] / 1000
+
+    # Criando o histograma
+    plt.figure(figsize=(12, 6))
+
+    # Usar a mesma paleta de cores do primeiro gráfico
+    cores = sns.color_palette('plasma', 20)
+
+    n, bins, patches = plt.hist(duracao_musica_segundos, bins=20, edgecolor='black')
+
+    # Atribuir cores diferentes a cada barra do histograma
+    for i in range(len(patches)):
+        patches[i].set_facecolor(cores[i])
+
+    # Adicionando rótulos e título ao gráfico
+    plt.xlabel('Duração da Música (segundos)', fontsize=17)
+    plt.ylabel('Frequência', fontsize=17)
+    plt.title('Duração das Músicas', fontsize=17)
+
+    # Ajustar o tamanho da fonte nos rótulos do eixo x
+    plt.xticks(ha='right', fontsize=12)
+
+    # Ajustar o tamanho da fonte nos rótulos do eixo y
+    plt.yticks(fontsize=12)
+
+    # Salvar o gráfico como imagem
+    plt.tight_layout()
+    plt.savefig('static/grafico14.png')
+
+    # Fechar a figura após salvar
+    plt.close()
